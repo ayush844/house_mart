17c5d417a1c98ce57831d814dad4cb8d2ef7e593
ayush844 <ayush.xyz1625@gmail.com>
2024-08-02 14:53:49 +0530

    getting params in server components
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,60 +1,31 @@
-'use client'
 
-import { useRouter, useParams, useSearchParams, usePathname } from "next/navigation";
-
-const PropertyPage = () => {
-    const router = useRouter();
-
-    const params = useParams();
-
-    const searchParams = useSearchParams();
-
-    const pathname = usePathname();
-
-
-
-    console.log(router)
-
-    // back: ()=>window.history.back()
-    // fastRefresh: ()=> {…}
-    // forward:()=>window.history.forward()
-    // prefetch: (href, options)=> {…}
-    // push:(href, options)=> {…}
-    // refresh: ()=> {…}
-    // replace: (href, options)=> {…}
-
-
-   // http://localhost:3000/properties/123456
-    console.log(typeof(params.id)); // string
-// we are using id because this is what is the name of the folder => [id]
-
-
-
-    console.log('Property page component');
+const PropertyPage = ({params, searchParams}) => {
 
     return ( <div>
 
+        particular property page
 
-        <h1>the param is {params.id}</h1> 
-        {/* http://localhost:3000/properties/123456 */}
+        {/* http://localhost:3000/properties/123456?name=ayush */}
+
+        <br />
+
+        {/* getting params in server component */}
+
+        {params.id}
+
         {/* 123456 */}
 
+        <br />
+
+        {/* getting search params in server component */}
+
+        {searchParams.name}
+
+        {/* ayush */}
 
 
-        <h1>the search param is {searchParams.get('name')}</h1>
-        {/* http://localhost:3000/properties/123456?name=ayush */}
-        {/* the search param is ayush */}
 
 
-
-        <h1>the pathname is {pathname}</h1>
-        {/* http://localhost:3000/properties/123456?name=ayush */}
-        {/* the pathname is /properties/123456 */}
-        
-
-        <button onClick={() => router.replace('/')}>
-            Go Home
-        </button>
     </div> );
 }
  
