a817254084dce58af31b1a9c9e83dd8f06540d9e
ayush844 <ayush.xyz1625@gmail.com>
2024-08-05 00:37:32 +0530

    made properties page and property card
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,9 +1,20 @@
+import ProperyCard from '@/components/PropertyCard';
+import properties from '@/properties.json'
 
 const PropertiesPage = () => {
 
-    return ( <div>
-        properties page
-    </div> );
+    return ( 
+    <section className=' px-4 py-6'>
+        <div className=' container-xl lg:container m-auto px-4 py-6'>
+            {properties.length === 0 ? (<p>No Properties Found</p>) : (
+                <div className=' grid grid-cols-1 md:grid-cols-3 gap-6'>
+                    {properties.map((property) => (
+                        <ProperyCard key={property._id} property={property} />
+                    ))}
+                </div>
+            )}
+        </div>
+    </section> );
 }
  
 export default PropertiesPage;
