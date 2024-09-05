ec4369562fb45b4ce1b5b7667402422db319f2fd
ayush844 <ayush.xyz1625@gmail.com>
2024-09-06 01:14:48 +0530

    saving user to database, adding profile image and sign out functionality from navbar
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,17 +10,47 @@ import defaultProfile from '@/assets/images/profile.png';
 import { usePathname } from 'next/navigation';
 
 import { FaGoogle } from "react-icons/fa";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Navbar = () => {
+
+  const {data: session} = useSession();
+
+  // console.log(session);
+
+//   Object
+// expires: "2024-10-05T18:22:48.899Z"
+// user: email: "ayush.xyz1625@gmail.com", id: "66d9e96bd91fa5b46ac8008c", image: "https://lh3.googleusercontent.com/a/ACg8ocJTY1qzHAF8UnKifeqhJm1oQ9uSOCrPk44XsGcPQntKdnzCOouH=s96-c", name: "Ayush Sharma"
+  
+
+
+  const profileImage = session?.user?.image;
+
 
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
     const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
-
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+   
+    const [providers, setProviders] = useState(null);
 
     const pathname = usePathname();
+
+
+    useEffect(()=>{
+      const setAuthProviders = async ()=>{
+
+        const res = await getProviders();
+        setProviders(res);
+
+
+      }
+
+      setAuthProviders();
+
+    }, []);
 
     return ( 
 
@@ -82,7 +112,7 @@ const Navbar = () => {
                     href="/properties"
                     className={`${pathname === '/properties' && "bg-black"} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                     >Properties</Link>
-                    {isLoggedIn && (
+                    {session && (
                         <Link
                         href="/properties/add"
                         className={`${pathname === '/properties/add' && "bg-black"} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
@@ -94,22 +124,28 @@ const Navbar = () => {
             </div>
   
             {/* Right Side Menu (Logged Out) */}
-            {!isLoggedIn && (
+            {!session && (
                         <div className="hidden md:block md:ml-6">
                             <div className="flex items-center">
-                              <button
-                                className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                              >
-                                <FaGoogle className=' text-white mr-2' />
-                                <span>Login or Register</span>
-                              </button>
+                              {providers && 
+                                Object.values(providers).map((provider, index) => (
+                                  <button key={index}
+                                  onClick={() => signIn(provider.id)}
+                                  className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                                >
+                                  <FaGoogle className=' text-white mr-2' />
+                                  <span>Login or Register</span>
+                                </button>
+                                ))
+                              }
+
                             </div>
                         </div>
             )}
 
   
             {/*Right Side Menu (Logged In)  */}
-            {isLoggedIn && (
+            {session && (
                             <div
                             className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0"
                           >
@@ -157,8 +193,10 @@ const Navbar = () => {
                                   <span className="sr-only">Open user menu</span>
                                   <Image
                                     className="h-8 w-8 rounded-full"
-                                    src={defaultProfile}
+                                    src={ profileImage || defaultProfile}
                                     alt=""
+                                    width={60}
+                                    height={60}
                                   />
                                 </button>
                               </div>
@@ -193,6 +231,10 @@ const Navbar = () => {
                                                     role="menuitem"
                                                     tabIndex="-1"
                                                     id="user-menu-item-2"
+                                                    onClick={() => {
+                                                      setIsProfileMenuOpen(false);
+                                                      signOut()
+                                                    }}
                                                   >
                                                     Sign Out
                                                   </button>
@@ -219,7 +261,7 @@ const Navbar = () => {
                         href="/properties"
                         className={`${pathname === '/properties' && "bg-black"}  text-white block rounded-md px-3 py-2 text-base font-medium`}
                         >Properties</Link>
-                        {isLoggedIn && (
+                        {session && (
                         <Link
                         href="/properties/add"
                         className={`${pathname === '/properties/add' && "bg-black"}  text-white block rounded-md px-3 py-2 text-base font-medium`}
@@ -227,7 +269,7 @@ const Navbar = () => {
                         )}
 
 
-                        {!isLoggedIn && (
+                        {!session && (
                             <button
                             className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
                             >
--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,5 +1,10 @@
 import GoogleProvider from 'next-auth/providers/google';
 
+import connectDB from '@/config/database';
+
+import User from '@/models/User';
+
+
 export const authOptions = {
     // Configure one or more authentication providers
     providers: [
@@ -17,25 +22,43 @@ export const authOptions = {
       
     ],
     callbacks: {
-        // invoke on sign in
-        async signIN({profile}){
+        // invoke on successfull sign in
+        async signIn({profile}){
+
             // connect to database
+            await connectDB();
 
             // check if user exists
+            const userExists = await User.findOne({email: profile.email});
 
             // if not, create user
+            if(!userExists){
+                // truncate username if too long
+                const username = profile.name.slice(0, 20);
+
+                await User.create({
+                    email: profile.email,
+                    username,
+                    image: profile.picture
+                })
+            }
+
 
             // return true to allow signIn
+            return true;
 
         },
 
         // session callback function that modifies session object
-        async session(){
+        async session({session}){
             // get user from database
+            const user = await User.findOne({email: session.user.email});
 
             // assign user id from the session
+            session.user.id = user._id.toString();
 
             // return session
+            return session;
             
         }
     }
