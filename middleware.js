a18c0e3b8e434ba42cdf5bd24e3f47713ca6409e
ayush844 <ayush.xyz1625@gmail.com>
2024-09-30 11:14:54 +0530

    increased payload size limit
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- middleware.js
+++ middleware.js
@@ -1,5 +1,19 @@
-export { default } from "next-auth/middleware"
+import { NextResponse } from 'next/server';
+import authMiddleware from 'next-auth/middleware'; // Import the default next-auth middleware
+
+export function middleware(req) {
+  const contentLength = req.headers.get('content-length') || 0;
+
+  const maxContentLength = 50 * 1024 * 1024; // 50 MB
+
+  if (parseInt(contentLength, 10) > maxContentLength) {
+    return new NextResponse('Payload too large', { status: 413 });
+  }
+
+  // Call next-auth middleware after content-length check
+  return authMiddleware(req);
+}
 
 export const config = {
-    matcher: ['/properties/add', '/profile', '/properties/saved', '/messages']
-}
+  matcher: ['/properties/add', '/profile', '/properties/saved', '/messages'],
+};
