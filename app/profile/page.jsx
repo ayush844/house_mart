15c1008ba7a8c5c7d067815b575971bc21911e28
ayush844 <ayush.xyz1625@gmail.com>
2024-09-30 12:20:45 +0530

    changed profile tet size
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -47,10 +47,10 @@ const ProfilePage = async() => {
                   />
                 </div>
   
-                <h2 className="text-2xl mb-4">
+                <h2 className="text-lg md:text-2xl mb-4">
                   <span className="font-bold block">Name: </span> {sessionUser.user.name}
                 </h2>
-                <h2 className="text-2xl">
+                <h2 className=" text-lg md:text-2xl">
                   <span className="font-bold block">Email: </span> {sessionUser.user.email}
                 </h2>
               </div>
